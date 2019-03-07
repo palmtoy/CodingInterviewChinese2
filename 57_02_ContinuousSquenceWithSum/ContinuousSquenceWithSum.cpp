6a1994c1f8f752eccf9d0f3c5084d703b2b267ba
李志刚 <ZhiGangLi@sf-express.com>
2019-03-07 15:05:58 +0800

    * optimize the code of 57_02_ContinuousSquenceWithSum

--- 57_02_ContinuousSquenceWithSum/ContinuousSquenceWithSum.cpp
+++ 57_02_ContinuousSquenceWithSum/ContinuousSquenceWithSum.cpp
@@ -7,17 +7,13 @@ Distributed under the BSD license.
 https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 *******************************************************************/
 
-//==================================================================
-// 《剑指Offer——名企面试官精讲典型编程题》代码
-// 作者：何海涛
-//==================================================================
-
 // 面试题57（二）：为s的连续正数序列
 // 题目：输入一个正数s，打印出所有和为s的连续正数序列（至少含有两个数）。
 // 例如输入15，由于1+2+3+4+5=4+5+6=7+8=15，所以结果打印出3个连续序列1～5、
 // 4～6和7～8。
 
 #include <cstdio>
+#include <algorithm>
 
 void PrintContinuousSequence(int small, int big);
 
@@ -69,6 +65,8 @@ void Test(const char* testName, int sum)
 
 int main(int argc, char* argv[])
 {
+    printf("57_02_ContinuousSquenceWithSum/ContinuousSquenceWithSum is running ...\n");
+
     Test("test1", 1);
     Test("test2", 3);
     Test("test3", 4);
